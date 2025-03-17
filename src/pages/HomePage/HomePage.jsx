cf3608448aebfe3c3ff7cb8fca558ee7c6c352c8
Roman <vrbox@i.ua>
2025-03-17 17:07:51 +0200

    Update HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,12 @@
 import MovieGalery from "../../components/MovieGalery/MovieGalery";
 import css from "./HomePage.module.css";
 
-export default function HomePage({movies, updatePage, showPage}) {
+export default function HomePage({movies}) {
 
     return (
         <div>
             <h1 className={css.headerTrending}>Trending Movies</h1>
-            <MovieGalery movies={movies} updatePage={updatePage} showPage={showPage} />
+            <MovieGalery movies={movies} />
         </div>
     );
 }
