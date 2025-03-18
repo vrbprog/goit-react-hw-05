11aaf03068cf6cb5d0620a82b57a4e6a45cfef14
vrbprog <vrbdsp@gmail.com>
2025-03-18 20:33:36 +0200

    Add default photo for cast

--- src/components/CastCard/CastCard.jsx
+++ src/components/CastCard/CastCard.jsx
@@ -1,16 +1,16 @@
-import css from './CastCard.module.css';
+import css from "./CastCard.module.css";
+import photo from "/src/assets/def_foto.png";
 
-export default function CastCard({path, name}) {
-    const img_url = `https://image.tmdb.org/t/p/w500${path}`;
+export default function CastCard({ path, name }) {
+    const getPhoto = () => {
+        if (path === null) return photo;
+        else return `https://image.tmdb.org/t/p/w500${path}`;
+    };
+
     return (
         <div className={css.castCard}>
-            <img
-                className={css.cardFoto}
-                alt={name}
-                src={img_url}
-             />
+            <img className={css.cardFoto} alt={name} src={getPhoto()} />
             <p className={css.name}>{name}</p>
         </div>
     );
-    
-};
+}
