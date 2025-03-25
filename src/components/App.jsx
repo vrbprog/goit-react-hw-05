c78f72bcf6c87db253a9d6ca48808159862a024f
Roman <vrbox@i.ua>
2025-03-25 16:23:54 +0200

    Upgrate wornings from mentor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,7 @@
 import { Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
 import HomePage from "../pages/HomePage/HomePage";
 // import MoviesPage from "../pages/MoviesPage/MoviesPage";
 import Navigation from "./Navigation/Navigation";
-import { getTrendingMovies } from "../services/themoviedb";
 // import MovieDetailsPage from "../pages/MovieDetailsPage/MovieDetailsPage";
 // import MovieCast from "../components/MovieCast/MovieCast";
 // import MovieReviews from "../components/MovieReviews/MovieReviews";
@@ -17,47 +15,23 @@ const MovieReviews = lazy(() => import("../components/MovieReviews/MovieReviews"
 const NotFoundPage = lazy(() => import("../pages/NotFoundPage/NotFoundPage"));
 
 export default function App() {
-    const [movies, setMovies] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isErrorLoading, setIsErrorLoading] = useState(false);
-
-    useEffect(() => {
-        const getTrending = async () => {
-            setIsErrorLoading(false);
-            setIsLoading(true);
-            try {
-                const data = await getTrendingMovies(1);
-                setMovies(data.results);
-            } catch {
-                setIsErrorLoading(true);
-            }
-            finally {
-                setIsLoading(false);
-            }
-        };
-        getTrending();
-    }, []);
-
     return (
         <div>
             <Navigation />
-            {isErrorLoading ? <h1 style={{ textAlign: "center", color: "red" }}>
-                Sorry, something went wrong...</h1> :
-                <Suspense fallback={<h1>Loading...</h1>}>
-                    <Routes>
-                        <Route path="/" element={<HomePage movies={movies} loading={isLoading} />} />
-                        <Route
-                            path="/movies/:moviesId"
-                            element={<MovieDetailsPage />}
-                        >
-                            <Route path="cast" element={<MovieCast />} />
-                            <Route path="reviews" element={<MovieReviews />} />
-                        </Route>
-                        <Route path="/movies" element={<MoviesPage />} />
-                        <Route path="*" element={<NotFoundPage />} />
-                    </Routes>
-                </Suspense>
-            }
+            <Suspense fallback={<h1>Loading...</h1>}>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route
+                        path="/movies/:moviesId"
+                        element={<MovieDetailsPage />}
+                    >
+                        <Route path="cast" element={<MovieCast />} />
+                        <Route path="reviews" element={<MovieReviews />} />
+                    </Route>
+                    <Route path="/movies" element={<MoviesPage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,39 @@
 import MovieList from "../../components/MovieList/MovieList";
 import css from "./HomePage.module.css";
+import { useEffect, useState } from "react";
+import { getTrendingMovies } from "../../services/themoviedb";
 
-export default function HomePage({movies, loading}) {
+export default function HomePage() {
+
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isErrorLoading, setIsErrorLoading] = useState(false);
+    
+        useEffect(() => {
+        const getTrending = async () => {
+            setIsErrorLoading(false);
+            setIsLoading(true);
+            try {
+                const data = await getTrendingMovies(1);
+                setMovies(data.results);
+            } catch {
+                setIsErrorLoading(true);
+            }
+            finally {
+                setIsLoading(false);
+            }
+        };
+        getTrending();
+    }, []);
 
     return (
-        <div>
-            <h1 className={css.headerTrending}>Trending Movies</h1>
-            <MovieList movies={movies} loading={loading} />
-        </div>
+        <>
+            {isErrorLoading ? <h1 className={css.errorLoading}>Sorry, something went wrong...</h1> :
+                <>
+                    <h1 className={css.headerTrending}>Trending Movies</h1>
+                    <MovieList movies={movies} loading={isLoading} />
+                </>
+            }
+        </>
     );
 }
